b715d63f23532d3da035ecb0f0a4315f42a345ba
15024444072 <1652288589@qq.com>
2017-11-20 23:13:09 +0800

    webpack多入口文件配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 //NodeJS中的Path对象，用于处理目录的对象，提高开发效率
 var path = require('path');
 var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');  //分离css单独打包插件
+var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');  //分离css单独打包插件
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: ['babel-polyfill', './src/pc.js'],//入口文件,字符串或数组或者是对象所表示的含义不一样
+        pc: ['babel-polyfill', './src/pc.js'],//入口文件,字符串或数组或者是对象所表示的含义不一样
         mobile: ['babel-polyfill', './src/mobile.js']
     },
     // output参数是个对象，用于定义构建后的文件的输出。
     output: {
         path: path.resolve(__dirname, './dist'),//定义用来存放打包后文件的输出目录
         publicPath: '/dist/',//指定资源文件引用的目录
-        filename: '[name].js',
+        filename: 'js/[name].js',
     },
     module: {
         //rules 来指定规则，它是一个数组，里面是每一个对象，对不同的文件指定具体的规则。
@@ -37,26 +37,25 @@ module.exports = {
             },
             {
                 test: /\.(scss|css)$/,
-                use: ExtractTextPlugin.extract({use: ['css-loader', 'sass-loader'], fallback: 'style-loader',})
+                use: ExtractTextWebpackPlugin.extract({use: ['css-loader', 'sass-loader'], fallback: 'style-loader',})
             },
         ]
     },
     //引入第三方插件
     plugins: [
-        new ExtractTextPlugin({
-            filename: 'index.css',
-            disable: false,
-            allChunks: true,
-        }),
+        new ExtractTextWebpackPlugin('css/[name].css'),
+
         new HtmlWebpackPlugin({
             template: './index.html',
             filename: 'index.html',
-            thunks: ['pc']
+            inject:false,  //所有静态资源css和JavaScript都不会自动注入到模板文件中，此处我们手动在html中添加
+            //chunks: ['pc'],
         }),
         new HtmlWebpackPlugin({
             template: './mobile.html',
             filename: 'mobile.html',
-            thunks: ['mobile']
+            inject:false,
+            //chunks: ['mobile'],
         }),
         new webpack.ProvidePlugin({
             jQuery: "jquery",
@@ -85,23 +84,23 @@ module.exports = {
     devtool: '#eval-source-map'
 }
 
-//if (process.env.NODE_ENV === 'production') {
-//    module.exports.devtool = '#source-map'
-//    // http://vue-loader.vuejs.org/en/workflow/production.html
-//    module.exports.plugins = (module.exports.plugins || []).concat([
-//        new webpack.DefinePlugin({
-//            'process.env': {
-//                NODE_ENV: '"production"'
-//            }
-//        }),
-//        new webpack.optimize.UglifyJsPlugin({
-//            sourceMap: true,
-//            compress: {
-//                warnings: false
-//            }
-//        }),
-//        new webpack.LoaderOptionsPlugin({
-//            minimize: true
-//        })
-//    ])
-//}
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ])
+}
