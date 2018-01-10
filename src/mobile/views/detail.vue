b0e0807c1f48128f67a2ca42c0cb43f5fb1e066a
15024444072 <1652288589@qq.com>
2018-01-10 11:20:15 +0800

    完善仿淘宝购物车功能

--- src/mobile/views/detail.vue
+++ src/mobile/views/detail.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="ccui-m-detail">
-        <div class="ccui-xwrap" style="background-color: #e7e7e7;">
+        <div class="ccui-xwrap" style="background-color: #e7e7e7;top:0;">
             <div class="ccui-xcontent">
-                <button style="height: 300px;" v-xevent.tap @tap="cxb()">324234234432</button>
-                <mt-swipe :show-indicators="false">
-                    <mt-swipe-item><img :src="product_img"></mt-swipe-item>
-                    <mt-swipe-item><img :src="product_img1"></mt-swipe-item>
-                    <mt-swipe-item><img :src="product_img2"></mt-swipe-item>
-                </mt-swipe>
+                <div class="ccui-swipe">
+                    <mt-swipe  :auto="0">
+                        <mt-swipe-item><img :src="product_img"></mt-swipe-item>
+                        <mt-swipe-item><img :src="product_img1"></mt-swipe-item>
+                        <mt-swipe-item><img :src="product_img2"></mt-swipe-item>
+                    </mt-swipe>
+                </div>
+              <div class="ccui-back"><i class="ccui-iconfont ccui-fanhui ccui-xfont36"></i></div>
             </div>
         </div>
 
@@ -18,7 +20,6 @@
     import Vue from 'vue';
     require('../assets/scss/detail.scss');
     import { Swipe, SwipeItem } from 'mint-ui';
-    import { Toast } from 'mint-ui';
     Vue.component(Swipe.name, Swipe);
     Vue.component(SwipeItem.name, SwipeItem);
 
@@ -26,21 +27,13 @@
     export default {
         data:function(){
             return {
-                product_img:require("mimgs/goods-3.jpg"),
+                product_img:require("mimgs/goods-4.jpg"),
                 product_img1:require("mimgs/goods-1.jpg"),
                 product_img2:require("mimgs/goods-2.jpg"),
             }
         },
         mounted:function(){},
         methods: {
-            cxb(){
-                Toast({
-                    message: '提示',
-                    position: 'middle',
-                    duration: 50000
-                });
-
-            },
         }
     }
 </script>
